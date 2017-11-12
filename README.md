b347270bf846a990279ed91a9bb32309d43b553d
GokulDas027 <gokuldas027@gmail.com>
2017-11-12 10:44:15 +0530

    SDS challenge Nov12 collatz

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # sds_weekly_challenge
 Weekly coding challenges by SDS.
+SDS stands for Student Developer Society, A Community based in Kochi and works on developing programming skills in students.
---
+++ collatz.py
@@ -0,0 +1,22 @@
+def collatz(num):
+    i = 1
+    while num != 1:
+        if num % 2 == 0:
+            num = num/2
+        else:
+            num = num*3+1
+        i += 1
+    return i
+
+
+r2 = 0
+result = 0
+large = 0
+for n in range(1, 1000000):
+    a = collatz(n)
+    if a > large:
+        large = a
+        r2 = result
+        result = n
+print("num with longest collatz : {0}".format(result))
+print("{0}".format(r2))
